a30e9e9e23e0aaae0b2cf15ce999016d4504138c
Telinc1 <2465873+telinc1@users.noreply.github.com>
2018-06-08 23:08:48 +0300

    Switch to Gson instead of json-simple

--- src/main/java/com/telinc1/faerie/sprite/emitter/JSONEmitter.java
+++ src/main/java/com/telinc1/faerie/sprite/emitter/JSONEmitter.java
@@ -22,14 +22,17 @@
 
 package com.telinc1.faerie.sprite.emitter;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonWriter;
 import com.telinc1.faerie.sprite.Sprite;
 import com.telinc1.faerie.sprite.SpriteBehavior;
 import com.telinc1.faerie.sprite.display.DisplayData;
 import com.telinc1.faerie.sprite.display.LabelDisplayData;
 import com.telinc1.faerie.sprite.display.SpriteTile;
 import com.telinc1.faerie.sprite.display.TileDisplayData;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 
 import java.io.IOException;
 import java.io.Writer;
@@ -51,116 +54,120 @@ public class JSONEmitter extends Emitter {
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public void emit(Writer writer) throws IOException{
         Sprite sprite = this.getSprite();
         SpriteBehavior behavior = sprite.getBehavior();
         DisplayData displayData = sprite.getDisplayData();
 
-        JSONObject json = new JSONObject();
-        json.put("Type", sprite.getType().asInteger());
-        json.put("SubType", sprite.getSubType().asInteger());
-        json.put("ActLike", sprite.getActsLike());
+        JsonObject json = new JsonObject();
+        json.addProperty("Type", sprite.getType().asInteger());
+        json.addProperty("Type", sprite.getType().asInteger());
+        json.addProperty("SubType", sprite.getSubType().asInteger());
+        json.addProperty("ActLike", sprite.getActsLike());
 
-        JSONObject $1656 = new JSONObject();
-        $1656.put("Object Clipping", behavior.objectClipping);
-        $1656.put("Can be jumped on", behavior.canBeJumpedOn);
-        $1656.put("Dies when jumped on", behavior.diesWhenJumpedOn);
-        $1656.put("Hop in/kick shell", behavior.hopInShells);
-        $1656.put("Disappears in cloud of smoke", behavior.disappearInSmoke);
-        json.put("$1656", $1656);
+        JsonObject $1656 = new JsonObject();
+        $1656.addProperty("Object Clipping", behavior.objectClipping);
+        $1656.addProperty("Can be jumped on", behavior.canBeJumpedOn);
+        $1656.addProperty("Dies when jumped on", behavior.diesWhenJumpedOn);
+        $1656.addProperty("Hop in/kick shell", behavior.hopInShells);
+        $1656.addProperty("Disappears in cloud of smoke", behavior.disappearInSmoke);
+        json.add("$1656", $1656);
 
-        JSONObject $1662 = new JSONObject();
-        $1662.put("Sprite Clipping", behavior.spriteClipping);
-        $1662.put("Use shell as death frame", behavior.useShellAsDeathFrame);
-        $1662.put("Fall straight down when killed", behavior.fallsWhenKilled);
-        json.put("$1662", $1662);
+        JsonObject $1662 = new JsonObject();
+        $1662.addProperty("Sprite Clipping", behavior.spriteClipping);
+        $1662.addProperty("Use shell as death frame", behavior.useShellAsDeathFrame);
+        $1662.addProperty("Fall straight down when killed", behavior.fallsWhenKilled);
+        json.add("$1662", $1662);
 
-        JSONObject $166E = new JSONObject();
-        $166E.put("Use second graphics page", behavior.useSecondGraphicsPage);
-        $166E.put("Palette", behavior.palette);
-        $166E.put("Disable fireball killing", behavior.disableFireballKilling);
-        $166E.put("Disable cape killing", behavior.disableCapeKilling);
-        $166E.put("Disable water splash", behavior.disableWaterSplash);
-        $166E.put("Don't interact with Layer 2", behavior.disableSecondaryInteraction);
-        json.put("$166E", $166E);
+        JsonObject $166E = new JsonObject();
+        $166E.addProperty("Use second graphics page", behavior.useSecondGraphicsPage);
+        $166E.addProperty("Palette", behavior.palette);
+        $166E.addProperty("Disable fireball killing", behavior.disableFireballKilling);
+        $166E.addProperty("Disable cape killing", behavior.disableCapeKilling);
+        $166E.addProperty("Disable water splash", behavior.disableWaterSplash);
+        $166E.addProperty("Don't interact with Layer 2", behavior.disableSecondaryInteraction);
+        json.add("$166E", $166E);
 
-        JSONObject $167A = new JSONObject();
-        $167A.put("Don't disable cliping when starkilled", behavior.processIfDead);
-        $167A.put("Invincible to star/cape/fire/bounce blk.", behavior.invincibleToPlayer);
-        $167A.put("Process when off screen", behavior.processWhileOffscreen);
-        $167A.put("Don't change into shell when stunned", behavior.skipShellIfStunned);
-        $167A.put("Can't be kicked like shell", behavior.disableKicking);
-        $167A.put("Process interaction with Mario every frame", behavior.processInteractionEveryFrame);
-        $167A.put("Gives power-up when eaten by Yoshi", behavior.isPowerup);
-        $167A.put("Don't use default interaction with Mario", behavior.disableDefaultInteraction);
-        json.put("$167A", $167A);
+        JsonObject $167A = new JsonObject();
+        $167A.addProperty("Don't disable cliping when starkilled", behavior.processIfDead);
+        $167A.addProperty("Invincible to star/cape/fire/bounce blk.", behavior.invincibleToPlayer);
+        $167A.addProperty("Process when off screen", behavior.processWhileOffscreen);
+        $167A.addProperty("Don't change into shell when stunned", behavior.skipShellIfStunned);
+        $167A.addProperty("Can't be kicked like shell", behavior.disableKicking);
+        $167A.addProperty("Process interaction with Mario every frame", behavior.processInteractionEveryFrame);
+        $167A.addProperty("Gives power-up when eaten by Yoshi", behavior.isPowerup);
+        $167A.addProperty("Don't use default interaction with Mario", behavior.disableDefaultInteraction);
+        json.add("$167A", $167A);
 
-        JSONObject $1686 = new JSONObject();
-        $1686.put("Inedible", behavior.inedible);
-        $1686.put("Stay in Yoshi's mouth", behavior.stayInMouth);
-        $1686.put("Weird ground behaviour", behavior.weirdGroundBehavior);
-        $1686.put("Don't interact with other sprites", behavior.disableSpriteInteraction);
-        $1686.put("Don't change direction if touched", behavior.preserveDirection);
-        $1686.put("Don't turn into coin when goal passed", behavior.disappearOnGoal);
-        $1686.put("Spawn a new sprite", behavior.spawnsSpriteWhenStunned);
-        $1686.put("Don't interact with objects", behavior.disableObjectInteraction);
-        json.put("$1686", $1686);
+        JsonObject $1686 = new JsonObject();
+        $1686.addProperty("Inedible", behavior.inedible);
+        $1686.addProperty("Stay in Yoshi's mouth", behavior.stayInMouth);
+        $1686.addProperty("Weird ground behaviour", behavior.weirdGroundBehavior);
+        $1686.addProperty("Don't interact with other sprites", behavior.disableSpriteInteraction);
+        $1686.addProperty("Don't change direction if touched", behavior.preserveDirection);
+        $1686.addProperty("Don't turn into coin when goal passed", behavior.disappearOnGoal);
+        $1686.addProperty("Spawn a new sprite", behavior.spawnsSpriteWhenStunned);
+        $1686.addProperty("Don't interact with objects", behavior.disableObjectInteraction);
+        json.add("$1686", $1686);
 
-        JSONObject $190F = new JSONObject();
-        $190F.put("Make platform passable from below", behavior.platformPassableFromBelow);
-        $190F.put("Don't erase when goal passed", behavior.ignoreGoal);
-        $190F.put("Can't be killed by sliding", behavior.disableSlideKilling);
-        $190F.put("Takes 5 fireballs to kill", behavior.takesFiveFireballs);
-        $190F.put("Can be jumped on with upwards Y speed", behavior.canBeJumpedOnFromBelow);
-        $190F.put("Death frame two tiles high", behavior.tallDeathFrame);
-        $190F.put("Don't turn into a coin with silver POW", behavior.ignoreSilverPSwitch);
-        $190F.put("Don't get stuck in walls (carryable sprites)", behavior.escapeWalls);
-        json.put("$190F", $190F);
+        JsonObject $190F = new JsonObject();
+        $190F.addProperty("Make platform passable from below", behavior.platformPassableFromBelow);
+        $190F.addProperty("Don't erase when goal passed", behavior.ignoreGoal);
+        $190F.addProperty("Can't be killed by sliding", behavior.disableSlideKilling);
+        $190F.addProperty("Takes 5 fireballs to kill", behavior.takesFiveFireballs);
+        $190F.addProperty("Can be jumped on with upwards Y speed", behavior.canBeJumpedOnFromBelow);
+        $190F.addProperty("Death frame two tiles high", behavior.tallDeathFrame);
+        $190F.addProperty("Don't turn into a coin with silver POW", behavior.ignoreSilverPSwitch);
+        $190F.addProperty("Don't get stuck in walls (carryable sprites)", behavior.escapeWalls);
+        json.add("$190F", $190F);
 
-        json.put("Extra Property Byte 1", sprite.getFirstPropertyByte());
-        json.put("Extra Property Byte 2", sprite.getSecondPropertyByte());
-        json.put("Unique Info", sprite.getUniqueByte());
+        json.addProperty("Extra Property Byte 1", sprite.getFirstPropertyByte());
+        json.addProperty("Extra Property Byte 2", sprite.getSecondPropertyByte());
+        json.addProperty("Unique Info", sprite.getUniqueByte());
 
-        json.put("AsmFile", sprite.usesFirstASM() ? sprite.getFirstASMFile() : "");
-        json.put("AsmFile2", sprite.usesSecondASM() ? sprite.getSecondASMFile() : "");
+        json.addProperty("AsmFile", sprite.usesFirstASM() ? sprite.getFirstASMFile() : "");
+        json.addProperty("AsmFile2", sprite.usesSecondASM() ? sprite.getSecondASMFile() : "");
 
-        json.put("Extra Bytes Length", sprite.getExtraBytes());
+        json.addProperty("Extra Bytes Length", sprite.getExtraBytes());
 
         if(displayData != null){
-            json.put("Name", displayData.getName());
+            json.addProperty("Name", displayData.getName());
 
-            JSONArray description = new JSONArray();
+            JsonArray description = new JsonArray();
             description.add(displayData.getDescription());
-            json.put("Description", description);
+            json.add("Description", description);
 
-            json.put("X", displayData.getPosition().x);
-            json.put("Y", displayData.getPosition().y);
+            json.addProperty("X", displayData.getPosition().x);
+            json.addProperty("Y", displayData.getPosition().y);
 
             if(displayData instanceof TileDisplayData){
-                JSONArray tiles = new JSONArray();
+                JsonArray tiles = new JsonArray();
                 TileDisplayData tileDisplayData = (TileDisplayData)displayData;
 
                 for(SpriteTile spriteTile : tileDisplayData.getTiles()){
-                    JSONObject tile = new JSONObject();
-                    tile.put("X", spriteTile.getPosition().x);
-                    tile.put("Y", spriteTile.getPosition().y);
-                    tile.put("Tile", spriteTile.getTile());
+                    JsonObject tile = new JsonObject();
+                    tile.addProperty("X", spriteTile.getPosition().x);
+                    tile.addProperty("Y", spriteTile.getPosition().y);
+                    tile.addProperty("Tile", spriteTile.getTile());
 
                     tiles.add(tile);
                 }
 
-                json.put("Tiles", tiles);
+                json.add("Tiles", tiles);
             }else if(displayData instanceof LabelDisplayData){
-                JSONArray label = new JSONArray();
+                JsonArray label = new JsonArray();
                 LabelDisplayData labelDisplayData = (LabelDisplayData)displayData;
 
                 label.add(labelDisplayData.getText());
-                json.put("Label", label);
+                json.add("Label", label);
             }
         }
 
-        writer.write(json.toJSONString());
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonWriter jsonWriter = gson.newJsonWriter(writer);
+        jsonWriter.setIndent("    ");
+        gson.toJson(json, jsonWriter);
+
         writer.flush();
     }
 }
